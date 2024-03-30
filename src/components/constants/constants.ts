38725e41d76ee1a7705db9259258d10fbeeb419b
AleksKen <aleks.ken2017@mail.ru>
2024-03-30 17:38:59 +0300

    Правки

--- src/components/constants/constants.ts
+++ src/components/constants/constants.ts
@@ -20,3 +20,13 @@ export const URL_MARKER_DEFAULT =
 
 //export const URL_MARKER_CURRENT =
 //  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+
+export const CITY_CARD_WIDTH = '260';
+
+export const CITY_CARD_HEIGHT = '200';
+
+export const REVIEW_SIZE = '54';
+
+export const BOOKMARK_ICON_WIDTH = '18';
+
+export const BOOKMARK_ICON_HEIGHT = '19';
