780f0c88b51f4fd1400822b4b7367b4d42d1f183
AleksKen <aleks.ken2017@mail.ru>
2024-04-20 20:30:09 +0300

    fix (?)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
+import axios, {AxiosError, AxiosInstance, AxiosResponse} from 'axios';
 import {getToken} from './token';
 import {StatusCodes} from 'http-status-codes';
 import {processErrorHandle} from './process-error-handle';
@@ -26,7 +26,7 @@ export const createAPI = (): AxiosInstance => {
   });
 
   api.interceptors.request.use(
-    (config: AxiosRequestConfig) => {
+    (config) => {
       const token = getToken();
 
       if (token && config.headers) {
